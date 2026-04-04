19662906a8930b22075ae10ec5ec96410537d968
Stanislav <stanislav-zabrodin@outlook.com>
2026-04-05 01:09:51 +0400

    updated cors for angular

--- hotelBooking_nest_back/src/main.ts
+++ hotelBooking_nest_back/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://10.8.144.42:3000", "http://localhost:4200"],
     credentials: true,
   });
 
