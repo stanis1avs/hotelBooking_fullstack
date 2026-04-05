ce23428077abfade761183bbb81ba24fbd6667cf
Stanislav <stanislav-zabrodin@outlook.com>
2026-04-05 15:51:31 +0400

    updated sidebar items

--- hotelBooking_angular_front/src/app/layout/shell/shell.ts
+++ hotelBooking_angular_front/src/app/layout/shell/shell.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { AuthActions } from '../../actions/auth.actions';
+import { selectAuthUser } from '../../selectors/auth.selectors';
 
 @Component({
   selector: 'app-shell',
   imports: [
+    AsyncPipe,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
@@ -17,7 +23,18 @@ import { MatButtonModule } from '@angular/material/button';
     MatListModule,
     MatIconModule,
     MatButtonModule,
+    MatDividerModule,
   ],
   templateUrl: './shell.html'
 })
-export class Shell {}
+export class Shell {
+  readonly user$;
+
+  constructor(private readonly store: Store) {
+    this.user$ = this.store.select(selectAuthUser);
+  }
+
+  logout() {
+    this.store.dispatch(AuthActions.logout());
+  }
+}
